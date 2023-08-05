5454c8b2c2f2405755671ef64e5a1bace2b3f86c
envelope <chaselonas@gmail.com>
2023-08-05 09:36:51 -0700

    added dockerfile for placeholder web app

--- webapp/src/app.ts
+++ webapp/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
